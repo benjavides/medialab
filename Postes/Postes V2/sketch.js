7607e469930d9b9260503dec6f5e0557d3c215da
benjavides <benjavides@gmail.com>
2016-04-08 13:01:47 -0300

    Creación de postes y cables funciona

--- Postes/Postes V2/sketch.js
+++ Postes/Postes V2/sketch.js
@@ -3,15 +3,17 @@
 //MEDIALAB 2016-1
 "use strict"
 var moving = false;
-var speed = 5;
+var speed = 10;
 var postes;
 var poste;
+var poste1;
+var frame=0;
 
 function setup() {
    createCanvas(400, 400);
    postes = [];
-   poste = new Poste();
-   poste.createNext(true);
+   poste1 = new Poste(); //primer poste, no tiene anterior
+   poste = poste1.createNext()
 }
 
 function draw() {
@@ -20,46 +22,38 @@ function draw() {
    postes.forEach(function(post) {
       post.move();
       post.display();
-      post.checkOutScreen();
-      //post.checkInScreen();
    });
-   //si hay 3 postes postes o menos crea uno con cierta probabilidad 
+   //si hay 4 postes postes o menos crea uno
    if (postes.length<=4){
-      probability(1/(frameRate()*1), poste.createNext.bind(poste)); //crea poste con probabilidad (lento, poco eficiente)
+      poste.createNext();
    }
+   if (frame%60==0){
+      checkDestroy();
+   }
+   frame+=1;
 }
 
 class Poste {
    constructor(prev) {
          this.prev = prev; //poste anterior
          this.cables = []; //lista de cables entre este poste y siguiente
-         this.pos = width; //posición x
+         if (this.prev == null){
+            this.pos = width;
+         }
+         else{
+            this.pos = random(this.prev.pos+width,this.prev.pos+2*width); //posición x
+         }
          this.size = createVector(20, 250);
          this.nextt = null; //siguiente poste
          postes.push(this); //agrega a lista de postes
          print("Poste creado");
       }
    //crea poste siguiente
-   createNext(first) {
-      if (this.prev==null){
-         print("esto solo se debería imprimir una vez");
-         poste = new Poste(this);
-         this.nextt = poste;
-         this.createCable(this,this.nextt);
-         //si hay 2 o menos postes crea uno
-         if (postes.length<=2){
-            this.createNext();
-         }
-      }
-      //crea poste solo si la separación es mayor al 50% de la pantalla
-      else if (this.pos<=width/2 | first == true){
-         poste = new Poste(this);
-         this.nextt = poste;
-         this.createCable();
-      }
-      else {
-         print("Lo intentaremos nuevamente");
-      }
+   createNext() {
+      poste = new Poste(this);
+      this.nextt = poste;
+      this.createCable();
+      return poste;
    }
    //crea cables entre este poste y el siguiente
    createCable() {
@@ -67,20 +61,25 @@ class Poste {
          new_cable = new Cable(this,this.nextt);
          this.cables.push(new_cable);
       }
-   //revisa si el poste está en pantalla y se destruye de ser asi
-   checkOutScreen(){
+   //revisa si el poste está en pantalla
+   checkOutScreen(){ 
       if (this.pos<this.size.x){
-         if (postes.length>=5){
-            postes.splice(postes.indexOf(this), 1);
-            print("Poste eliminado. Quedan:");
-            print(postes.length);
-         }
+         return true;
       }
    }
-   //revisa si está en pentalla, de ser asi y no tener poste a continuación lo crea
+   //se destruye el poste
+   destroy(){
+      postes.splice(postes.indexOf(this), 1);
+      print("Poste eliminado. Quedan:");
+      print(postes.length);
+   }
+   //revisa si está en pantalla
    checkInScreen(){
-      if ((0<=this.pos<=width) && this.nextt==null){
-         this.createNext();
+      if (0<=this.pos){
+         return true;
+      }
+      else {
+         return false;
       }
    }
    //mueve el poste a la izquierda
@@ -107,7 +106,7 @@ class Cable {
    constructor(poste_start,poste_finish) {
       this.poste_start=poste_start;
       this.poste_finish=poste_finish;
-      this.yvalues=new Array(abs(this.poste_finish.pos-this.poste_start.pos));
+      this.yvalues=new Array(abs(int(this.poste_finish.pos-this.poste_start.pos)));
       this.calcCurve();
    }
    //calcula el cable
@@ -150,5 +149,15 @@ function probability(prob, funct) {
       funct();
    } 
    else {
+   }
+}
+//chequea si hay que destruir el poste más antiguo
+function checkDestroy(){
+   let first;
+   first = postes[0];
+   if (!first.checkInScreen()){
+         if (first.nextt != null &&  !first.nextt.checkInScreen()){ //Lo elimina si el poste y su sucesor están fuera de pantalla
+            first.destroy();
+         }
    }
 }
